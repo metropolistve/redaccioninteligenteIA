6504ca2c42ed9ef50b09c4663990b8d97ebe4d6e
metropolistve <metropolis.tve@gmail.com>
2025-07-19 03:06:49 +0200

    Add files via upload

---
+++ js/glosario.js
@@ -0,0 +1,20 @@
+
+async function cargarGlosario() {
+  const response = await fetch('data/glosario.json');
+  const data = await response.json();
+  const select = document.getElementById('terminos');
+  data.forEach(item => {
+    const option = document.createElement('option');
+    option.value = item.Término;
+    option.textContent = item.Término;
+    select.appendChild(option);
+  });
+  window.definiciones = data;
+}
+function mostrarDefinicion() {
+  const termino = document.getElementById('terminos').value;
+  const div = document.getElementById('definicion');
+  const definicion = window.definiciones.find(item => item.Término === termino);
+  div.innerHTML = definicion ? `<strong>${termino}</strong>: ${definicion.Definición}` : "";
+}
+cargarGlosario();
